ae8bcdb724bc52f48033a9c642ce4e4b54e0c913
liangyanbin <1144274802@qq.com>
2022-10-17 15:08:47 +0800

    jetpack navigate demo

---
+++ jetpackNavigate/app/src/main/java/com/example/jetpack/navigate/ui/activity/LoginActivity.kt
@@ -0,0 +1,12 @@
+package com.example.jetpack.navigate.ui.activity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.jetpack.navigate.R
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_login)
+    }
+}
